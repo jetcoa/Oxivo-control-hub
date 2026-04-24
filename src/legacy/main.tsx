4e63bdcf1439cba591736607ce568fada1171641
Andrej <andrej@local>
2026-04-24 05:58:45 +0530

    fix(legacy): point legacy entry to Operators Hub

--- src/legacy/main.tsx
+++ src/legacy/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import AgentCenterLegacy from "@/legacy/pages/AgentCenterLegacy";
+import OperatorHub from "@/pages/OperatorHub";
 import "../index.css";
 
-createRoot(document.getElementById("root")!).render(<AgentCenterLegacy />);
+createRoot(document.getElementById("root")!).render(<OperatorHub />);
